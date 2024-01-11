aa3243297f16c0032ba196a4235775e7ec85bf11
aaziz2023 <144838290+aaziz2023@users.noreply.github.com>
2024-01-11 20:18:26 +0100

    Add files via upload

---
+++ ahmed.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ahmed {
+
+    public static void main (String[] args) {
+
+        for(int i=1; i<=5; i++){
+    for( int l = 1; l<=5; l++){
+        System.out.print("*");
+    }
+    System.out.println();
+
+}
+
+    }
+ {
+
+        for(int i=1; i<=5; i++){
+            System.out.println(" ");
+    for( int l = 1; l<=5; l++){
+        System.out.print("*");
+    }
+    if (l=1 || l==5);{
+    System.out.println();
+
+}
+
+    }
+}
+}

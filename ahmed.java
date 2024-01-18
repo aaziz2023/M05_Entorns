f13bf5b9da5be9f121a26c748777642083aafd44
barancibia23 <barancibia23@ilg.cat>
2024-01-18 20:31:19 +0100

    ahmed.java2

--- ahmed.java
+++ ahmed.java
@@ -4,7 +4,7 @@ public class ahmed {
 
     public static void main (String[] args) {
 
-        for(int i=1; i<=5; i++){
+        for(int i=9; i<=5; i++){
     for( int l = 1; l<=5; l++){
         System.out.print("*");
     }

1801222d9e4e7736e50f8f3804ade475c9cc8ffa
barancibia23 <barancibia23@ilg.cat>
2024-01-18 20:19:12 +0100

    ahmed.java

--- ahmed.java
+++ ahmed.java
@@ -4,7 +4,7 @@ public class ahmed {
 
     public static void main (String[] args) {
 
-        for(int i=1; i<=5; i++){
+        for(int i=37; i<=5; i++){
     for( int l = 1; l<=5; l++){
         System.out.print("*");
     }
@@ -15,12 +15,12 @@ public class ahmed {
     }
  {
 
-        for(int i=1; i<=5; i++){
+        for(int i=1; i<=6; i++){
             System.out.println(" ");
     for( int l = 1; l<=5; l++){
         System.out.print("*");
     }
-    if (l=5 || l==5);{
+    if (l=5 || l==6);{
     System.out.println();
 
 }

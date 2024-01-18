bdfb2d73f6509754dfccd76c5fd5766a4f270a52
ahmed aziz <aaziz2023@ilg.cat>
2024-01-18 20:17:49 +0100

    ahmed.java.2

--- ahmed.java
+++ ahmed.java
@@ -4,7 +4,7 @@ public class ahmed {
 
     public static void main (String[] args) {
 
-        for(int i=1; i<=5; i++){
+        for(int i=39; i<=5; i++){
     for( int l = 1; l<=5; l++){
         System.out.print("*");
     }
@@ -20,7 +20,7 @@ public class ahmed {
     for( int l = 1; l<=5; l++){
         System.out.print("*");
     }
-    if (l=5 || l==5);{
+    if (l=5 || l==7);{
     System.out.println();
 
 }
